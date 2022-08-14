4b2b307b44595d436bb84a79c35642e3096357ec
mysssssss <danila.2565@gmail.com>
2022-08-13 21:18:21 -0400

    intial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const connectDB = require("./config/db");
+
+// load config
+dotenv.config({ path: "./config/config.env" });
+
+// passport config
+require("./config/passport")(passport);
+
+connectDB();
+
+const app = express();
+
+// body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// method-override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+// logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// handlebars helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
+
+//handlebars
+app.set("view engine", ".hbs");
+app.engine(
+  "hbs",
+  exphbs.engine({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+
+// sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
